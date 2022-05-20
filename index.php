9ad65b3527d9abe876875abb1e262777ee383f78
Abiral <alwaysme125@gmail.com>
2022-05-20 09:37:28 +0545

    Hospital Project

---
+++ index.php
@@ -0,0 +1,152 @@
+ <?php
+ header("Location:login/");
+include('components/header.php');
+?>
+<!-- 
+<div class="mainbody">
+<div class="criticalUrgency">
+   <div class="title"> 
+       <h3><p> Covid Update</p> </h3>
+   </div>
+   <div class="urgencydatablock">
+       <div class="latestUpdate">
+       <div class="subtitle ">
+           <p>Last 24 hours update</p>
+           <p>Updated on 14 Dec 2021</p>
+           
+       </div>
+       <div class="subtitle ">
+           <p>Total Data</p>
+           <p>Updated on 14 Dec 2021</p>
+       </div>
+       
+       </div>
+       
+
+   </div>
+   <div class="databox first">
+   <div class="databox row">
+             <div class="box datablue">
+                 
+             <div class="boxtitle">
+                     New Cases
+                 </div>
+                    <div class="databluevalue">
+                        100
+                    </div>
+             </div>
+             <div class="box datagreen">
+                 
+             <div class="boxtitle">
+                     New Cases
+                 </div>
+                    <div class="databluevalue">
+                        100
+                    </div>
+             </div>
+             <div class="box datared">
+             <div class="boxtitle">
+                     New Cases
+                 </div>
+                    <div class="databluevalue">
+                        100
+                    </div>
+             </div>  
+    </div>
+    <div class="databox second">
+             <div class="box datablue">
+                 <div class="boxtitle">
+                     New Cases
+                 </div>
+                    <div class="databluevalue">
+                        100
+                    </div>
+             </div>
+             <div class="box datagreen">
+             <div class="boxtitle">
+                     Recovered
+                 </div>
+                 <div class="databluevalue">
+                        100
+                    </div>
+             </div>
+             <div class="box datared">
+             <div class="boxtitle">
+                     Death
+                 </div>
+                 <div class="databluevalue">
+                        100
+                    </div>
+             </div>  
+    </div>
+    </div>
+</div>
+
+<div class="urgentnotice">
+<div class="title"> 
+       <h3><p> Critical Urgency</p> </h3>
+   </div>
+
+
+<div class="slideshow">
+<?php //include('components/slider.html') ?>
+</div>
+
+
+
+
+<span class="seeMore button"><a href="">See more</a></span>
+
+</div>
+
+
+<div class="container">
+    <div class="containerimage">
+    <img src="images/help.svg" alt="Help">
+    </div> 
+    <div class="containertext">
+        <h1><p>Give</p>
+            <p>Your</p>
+            <p>hands</p>
+    </h1>
+    <div class="button donate">DONATE</div>
+    </div> 
+</div>  
+    <div class="container">
+        <div class="containertext">
+            <h1><p>Consult</p>
+            <p>With</p>
+            <p>Doctors</p>
+        </h1>
+        <div class="button consult">CONSULT</div>
+    </div>   
+    <div class="containerimage">
+    <img src="images/help.svg" alt="Help">
+    </div> 
+
+</div>
+
+<div class="title">
+       <h3> <p> Hospitals </p> </h3>
+</div> 
+
+<div class="slideshow">
+<?php //include('components/sliderAmbulance.html') ?>
+</div>
+
+<div class="title">
+       <h3> <p> Hospitals </p> </h3>
+</div> 
+
+<div class="slideshow">
+<?php //include('components/sliderUrgency.html') ?>
+</div>
+
+    
+
+
+       <script type="text/javascript" src="js/script.js"></script>
+
+<?php //include('components/footer.php')?>
+</body>
+</html>  -->
